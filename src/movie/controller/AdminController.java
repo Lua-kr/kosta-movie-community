7ea856528b66514fcde4df56d5ba0df6aad14a7b
boshy <boshy@192.168.0.190>
2020-11-06 12:10:49 +0900

    AdminController 수정 -지훈

--- src/movie/controller/AdminController.java
+++ src/movie/controller/AdminController.java
@@ -9,8 +9,69 @@ public class AdminController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		return null;
 	}
-
 	
+	/**
+	 * 	유저의 관리자 여부 확인
+	 * */
+	public ModelAndView checkAdmin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+	/**
+	 * 	회원 정보 (검색)
+	 * */
+	public ModelAndView AdminSearchuㅛ(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+
+	/**
+	 * 	회원 정보 (삭제)
+	 * */
+	public ModelAndView AdminDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	회원 블랙 리스트 등록(아이디 차단)
+	 * */
+	public ModelAndView AdminBlackList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	게시물 (수정)
+	 * */
+	public ModelAndView AdminBoardUpdate(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	게시물 (삭제)
+	 * */
+	public ModelAndView AdminBoardDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	포인트 상점 관리(추가)
+	 * */
+	public ModelAndView AdminPointInsert(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	포인트 상점 관리(수정)
+	 * */
+	public ModelAndView AdminPointUpdate(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
+	
+	/**
+	 * 	포인트 상점 관리(삭제)
+	 * */
+	public ModelAndView AdminPointDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return null;
+	}
 	
 	
 	
