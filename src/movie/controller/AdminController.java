c845a6b33a8859196459695d853d915baadda5b0
User <User@192.168.0.197>
2020-11-06 15:03:11 +0900

    AdminController (AdminBlackList,AdminBoardUpdate,AdminBoardDelete) - 지현

--- src/movie/controller/AdminController.java
+++ src/movie/controller/AdminController.java
@@ -1,7 +1,12 @@
 package movie.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import movie.dto.UserDTO;
+import movie.service.AdminService;
 
 public class AdminController implements Controller {
 
@@ -14,42 +19,79 @@ public class AdminController implements Controller {
 	 * 	유저의 관리자 여부 확인
 	 * */
 	public ModelAndView checkAdmin(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
-	}
+		String uid = request.getParameter("uid");
+		AdminService.checkAdmin(Integer.parseInt(uid));
+		
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
+	}//checkAdmin
 
 	/**
 	 * 	회원 정보 (검색)
 	 * */
-	public ModelAndView AdminSearchuㅛ(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
-	}
+	public ModelAndView AdminSearch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		List<UserDTO> list = AdminService.AdminSearch();
+		request.setAttribute("list", list);
+		
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
+	}//AdminSearch
 
 	/**
 	 * 	회원 정보 (삭제)
 	 * */
 	public ModelAndView AdminDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
-	}
+		String uid = request.getParameter("uid");
+		
+		AdminService.AdminDelete(Integer.parseInt(uid));
+		
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
+	}//AdminDelete
 	
 	/**
 	 * 	회원 블랙 리스트 등록(아이디 차단)
 	 * */
 	public ModelAndView AdminBlackList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+		
+		
+		String uid = request.getParameter("uid");
+		String locked = request.getParameter("ACCOUNT_LOCKED");
+		
+		AdminService.AdminBlackList(Integer.parseInt(uid),Integer.parseInt(locked));
+		ModelAndView mv = new ModelAndView();
+		
+		return mv;
 	}
 	
 	/**
 	 * 	게시물 (수정)
 	 * */
 	public ModelAndView AdminBoardUpdate(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+
+		String uid = request.getParameter("U_ID");
+		String text = request.getParameter("TEXT");
+		String title = request.getParameter("TITLE");
+				
+		AdminService.AdminBoardUpdate(Integer.parseInt(uid), text, title);
+		ModelAndView mv = new ModelAndView();
+		return mv;
 	}
 	
 	/**
 	 * 	게시물 (삭제)
 	 * */
 	public ModelAndView AdminBoardDelete(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+		
+		String uid =request.getParameter("U_ID");
+		//String path = request.getServletContext().getRealPath("/save");
+		
+		AdminService.AdminBoardDelete(Integer.parseInt(uid));
+		ModelAndView mv = new ModelAndView();
+		return mv;
 	}
 	
 	/**
